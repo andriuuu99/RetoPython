992c7dc1b5ee5bbc83622dd9a9d9e8ad3216a06a
Andrés Rodríguez <arr1006@alu.ubu.es>
2024-03-02 10:08:18 +0100

    #6 Caso 6 Resuelto

--- codigoCaso6.py
+++ codigoCaso6.py
@@ -1,14 +1,20 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 import pandas as pd
-
 
 app = Flask(__name__)
 
-# Lee el archivo CSV con los datos de los vehículos
-df = pd.read_csv('caso5.csv')
 
 @app.route('/<matricula>', methods=['GET'])
-def obtener_ultima_posicion(matricula):
+def ObtenerFechaUltimaPosicion(matricula):
+    '''
+    Obtiene la fecha de la última posición de un vehículo según su matricula.
+
+    @param matricula Matricula del vehículo
+    
+    '''
+    # Lee el archivo CSV con los datos de los vehículos
+    df = pd.read_csv('caso5.csv')
+
     # Filtra el DataFrame por la matrícula dada
     vehiculo = df[df['Matricula'] == matricula]
     
@@ -16,12 +22,10 @@ def obtener_ultima_posicion(matricula):
     if vehiculo.empty:
         return jsonify({'mensaje': 'No se encontraron datos para la matrícula especificada'}), 404
     
-    # Obtiene la posición del vehículo
-    ultima_posicion = vehiculo[0]  
-    
     # Devuelve la fecha de la última posición
-    fecha_ultima_posicion = ultima_posicion['Fecha']
+    fecha_ultima_posicion = df.iloc[0]['Fecha']
     return jsonify({'fecha_ultima_posicion': fecha_ultima_posicion}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
